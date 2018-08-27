9f264e319a4279843e5c4ae7bbf35b24d4fb3fcc
dalley <senghuotlay86@gmail.com>
2018-08-27 23:07:11 +1000

    Added basic registration form and allows registration through firebase

--- DocComm/app/src/main/java/au/edu/uts/doccomm/MainActivity.java
+++ DocComm/app/src/main/java/au/edu/uts/doccomm/MainActivity.java
@@ -1,13 +1,110 @@
 package au.edu.uts.doccomm;
 
+import android.app.ProgressDialog;
+import android.nfc.Tag;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnRegister;
+    private EditText etEmail;
+    private EditText etPassword;
+    private TextView tvSignIn;
+
+    private ProgressDialog progressDialog;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        progressDialog = new ProgressDialog(this);
+
+        btnRegister = (Button) findViewById(R.id.btnRegister);
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        tvSignIn = (TextView) findViewById(R.id.tvSignIn);
+
+        btnRegister.setOnClickListener(this);
+        tvSignIn.setOnClickListener(this);
+    }
+
+    /* TODO: Implement if the user has already been signed in
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+
+    */
+    private void registerUser(){
+        String emailAddress = etEmail.getText().toString().trim();
+        String password = etPassword.getText().toString().trim();
+
+        //Validation method that ensures that the user has entered email and password to register
+        //When the user did not enter anything to the email field
+        //Stop the except and provide an exception error
+        if(TextUtils.isEmpty(emailAddress)) {
+            Toast.makeText(this,"Please enter your email", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(password)) {
+            Toast.makeText(this,"Please enter your password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+        //If the validation is successful, show registration progress
+        progressDialog.setMessage("Registering in Process, Please Wait");
+        progressDialog.show();
+
+        mAuth.createUserWithEmailAndPassword(emailAddress,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            //Sign in success, update UI with the signed-in
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(MainActivity.this,"Registration Successful",Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            Toast.makeText(MainActivity.this,"Registration Unsuccessful",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+
+
+    @Override
+    public void onClick(View view) {
+        if (view == btnRegister) {
+            registerUser();
+        }
+
+        if (view == tvSignIn) {
+            //TODO: Redirect User to Sign In Page
+        }
     }
 }
