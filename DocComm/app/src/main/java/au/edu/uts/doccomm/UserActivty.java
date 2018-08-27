caf997cf5f76b31882c8394382f55ccfe0689d0e
dalley <senghuotlay86@gmail.com>
2018-08-28 01:40:59 +1000

    Added Firebase Database Storage API to build gradle. TODO: Add the user information to the database storage once registered

--- DocComm/app/src/main/java/au/edu/uts/doccomm/UserActivty.java
+++ DocComm/app/src/main/java/au/edu/uts/doccomm/UserActivty.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.StorageReference;
 
 public class UserActivty extends AppCompatActivity implements View.OnClickListener {
 
