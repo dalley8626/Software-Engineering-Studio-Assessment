fe221cc046dcc6b685df81dc60ff85400b8cb37c
jctrulz1 <jctrulz1@fastmail.fm>
2018-10-01 18:33:49 +1000

    doctor can view the profile of patients

--- DocComm/app/src/main/java/au/edu/uts/doccomm/ViewPatientProfileFromDoctor.java
+++ DocComm/app/src/main/java/au/edu/uts/doccomm/ViewPatientProfileFromDoctor.java
@@ -1,10 +1,20 @@
 package au.edu.uts.doccomm;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Map;
 
 public class ViewPatientProfileFromDoctor extends AppCompatActivity {
 
@@ -14,6 +24,9 @@ public class ViewPatientProfileFromDoctor extends AppCompatActivity {
     TextView heightTV;
     TextView weightTV;
     TextView medicalConditionTV;
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
 
     public void receivedDataPackets(View view) {
         Intent intent = new Intent(getApplicationContext(), DataPacketListFromDoctor.class);
@@ -26,6 +39,9 @@ public class ViewPatientProfileFromDoctor extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_patient_profile_from_doctor);
 
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference("users");
+
         nameTV = findViewById(R.id.viewNameTV2);
         genderTV = findViewById(R.id.viewGenderTV2);
         heightTV = findViewById(R.id.viewHeightTV2);
@@ -33,5 +49,22 @@ public class ViewPatientProfileFromDoctor extends AppCompatActivity {
         medicalConditionTV = findViewById(R.id.viewMedicalConditionTV2);
 
         patientID = getIntent().getStringExtra("patientID");
+
+        mDatabase.child(patientID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Map<String, Object> user = (Map<String, Object>) dataSnapshot.getValue();
+                nameTV.setText((String) user.get("firstName") + user.get("lastName"));
+                genderTV.setText((String) user.get("gender"));
+                heightTV.setText((String) user.get("height"));
+                weightTV.setText((String) user.get("weight"));
+                medicalConditionTV.setText((String) user.get("medicalCondition"));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
