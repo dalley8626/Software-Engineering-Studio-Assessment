2a071d16d3d7c01824749cac2549197fa4b7a68d
jctrulz1 <jctrulz1@fastmail.fm>
2018-10-01 17:01:38 +1000

    added a logout button for doctor activity

--- DocComm/app/src/main/java/au/edu/uts/doccomm/DoctorActivity.java
+++ DocComm/app/src/main/java/au/edu/uts/doccomm/DoctorActivity.java
@@ -4,8 +4,19 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class DoctorActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    public void logout(View view) {
+        mAuth.signOut();
+        finish();
+        startActivity(new Intent(this, LoginActivity.class));
+    }
 
     public void viewPatients(View view) {
         Intent intent = new Intent(getApplicationContext(), ViewPatientsActivity.class);
@@ -16,5 +27,7 @@ public class DoctorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor);
+
+        mAuth = FirebaseAuth.getInstance();
     }
 }
