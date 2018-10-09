3f6240460985069fbccba9fa91ebc627ebed5f1f
Cario Tan <cario.tan@outlook.com>
2018-10-09 17:27:02 +1100

    slfjldskfjdsjfklds

--- DocComm/app/src/main/java/au/edu/uts/doccomm/newDataPacketsFromDoctor.java
+++ DocComm/app/src/main/java/au/edu/uts/doccomm/newDataPacketsFromDoctor.java
@@ -1,7 +1,12 @@
 package au.edu.uts.doccomm;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -12,6 +17,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 public class newDataPacketsFromDoctor extends AppCompatActivity {
 
@@ -19,11 +25,20 @@ public class newDataPacketsFromDoctor extends AppCompatActivity {
     private DatabaseReference mDatabase;
 
     private ArrayList<String> recentDataPackets;
+    private ArrayList<String> dataPacketIDs;
+    private ArrayList<String> patientIDs;
+    private ArrayList<String> timestamps;
 
     String patientID;
     String doctorID;
 
     ListView recentDataPacketsLV;
+
+
+    private String mapToString(Map<String, Object> dataPacket) {
+            return (String) dataPacket.get("name") + " " + "\n" +
+                    dataPacket.get("timestamp");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,38 +48,49 @@ public class newDataPacketsFromDoctor extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         mDatabase = FirebaseDatabase.getInstance().getReference("users");
 
-        doctorID = mAuth.getCurrentUser().getUID();
+        doctorID = mAuth.getCurrentUser().getUid();
 
         recentDataPackets = new ArrayList<>();
+        patientIDs = new ArrayList<>();
+        dataPacketIDs = new ArrayList<>();
+        timestamps = new ArrayList<>();
 
         recentDataPacketsLV = findViewById(R.id.recentDataPacketsLV);
 
-        mDatabase.addValueEventListenener(
-                new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        for(DataSnapshot snapshot : dataSnapshot.child(doctorID).child("recentDataPackets")) {
-                            Map<String, Object> dataPacket = new HashMap<>();
-                            dataPacket = (Map<String, Object>) snapshot.getValue();
-                            String userID = dataPacket.get("userID");
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, recentDataPackets);
 
-                            if(snapshot != null) {
-                                recentDataPacketsLV.setText("This is an error. Please Fix");
-                            }
-                            else {
-
-                                break;
-                            }
-                        }
-
-                    }
-
-                    @Override
-                    public void dataCancelled(DatabaseError databaseError) {
-                        //This is meant to do nothing.
-                    }
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot snapshot : dataSnapshot.child(doctorID).child("recentDataPackets").getChildren()) {
+                    String packetID = snapshot.getKey();
+                    Map<String, Object> dataPacket = (Map<String, Object>) snapshot.getValue();
+                    recentDataPackets.add(mapToString(dataPacket));
+                    dataPacketIDs.add(packetID);
+                    patientIDs.add((String) dataPacket.get("userID"));
+                    timestamps.add((String) dataPacket.get("timestamp"));
                 }
-        );
+
+                recentDataPacketsLV.setAdapter(arrayAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        recentDataPacketsLV.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), DataPacketViewDoctor.class);
+                intent.putExtra("patientID", patientIDs.get(position));
+                intent.putExtra("doctorID", doctorID);
+                intent.putExtra("timeStamp", timestamps.get(position));
+
+                startActivity(intent);
+            }
+        });
 
     }
 }
